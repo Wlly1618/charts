941bbe9f92e8c990903cb7476413253a88899538
swilfredojoel <swilfredojoel@gmail.com>
2024-11-10 00:30:02 -0300

    chart running fine

--- chart.js
+++ chart.js
@@ -2,42 +2,42 @@ import { get_data, get_column, get_keys } from './data.js';
 
 const call_charts = async () => {
   const dataset = await get_data('./dataset_ventas.csv');
-
+  console.log(dataset)
   render_main_chart(dataset);
 }
 
-const groupByTime = (data, times) => {
-  const groupedData = times.reduce((acc, time) => {
+const group_by_time = (data, times) => {
+  const grouped = times.reduce((acc, time) => {
     acc[time] = [];
     return acc;
   }, {});
 
   data.forEach(item => {
-    const saleTime = item.time.split(':')[0] + ':00:00';
-    if (groupedData[saleTime]) {
-      groupedData[saleTime].push(item);
+    const sale_time = item.time.split(':')[0] + ':00:00';
+    if (grouped[sale_time]) {
+      grouped[sale_time].push(item);
     }
   });
 
-  return groupedData;
+  return grouped;
 };
 
 const colors = [
-  "rgb(52, 73, 94)",
-  "rgb(218, 247, 166)",
-  "rgb(255, 195, 0)",
-  "rgb(255, 87, 51)",
-  "rgb(199, 0, 57)",
-  "rgb(144, 12, 63)",
-  "rgb(52, 73, 94)",
-  "rgb(218, 247, 166)",
-  "rgb(255, 195, 0)",
-  "rgb(255, 87, 51)",
-  "rgb(199, 0, 57)",
-  "rgb(144, 12, 63)",
-  "rgb(52, 73, 94)",
-  "rgb(218, 247, 166)",
-  "rgb(255, 195, 0)",
+  'rgb(128, 222, 234)',
+  'rgb(179, 229, 252)',
+  'rgb(129, 212, 250)',
+  'rgb(121, 134, 203)',
+  'rgb(149, 117, 205)',
+  'rgb(240, 98, 146)',
+]
+
+const bg_colors = [
+  'rgba(128, 222, 234, .2)',
+  'rgba(179, 229, 252, .2)',
+  'rgba(129, 212, 250, .2)',
+  'rgba(121, 134, 203, .2)',
+  'rgba(149, 117, 205, .2)',
+  'rgba(240, 98, 146, .2)',
 ]
 
 const render_main_chart = (dataset) => {
@@ -47,42 +47,64 @@ const render_main_chart = (dataset) => {
     '20:00:00', '21:00:00', '22:00:00',
   ];
   
-  const groupedSales = groupByTime(dataset, times);
+  const groupedSales = group_by_time(dataset, times);
 
-  const total = () => {
+  const get_totals = (data, arg) => {
     const array = [];
     times.forEach(time => {
       let temp = 0;
-      groupedSales[time].forEach(element => {
-        temp += parseFloat(element.price);
+      data[time].forEach(element => {
+        if (arg == 0) temp += parseFloat(element.amount);
+        if (arg == 1) temp += parseFloat(element.price);
       });
-      array.push(temp);
+      array.push(parseInt(temp));
     })
 
     return array;
   }
 
+  const prods = get_totals(groupedSales, 0);
+  const prices = get_totals(groupedSales, 1);
+
   const data = {
     labels: times,
     datasets: [
       {
-        data: total(),
-        backgroundColor: colors,
-        borderColor: "rgba(0,0,255,0.1)",
+        label: 'Ventas',
+        data: prices,
+        backgroundColor: bg_colors[4],
+        borderColor: colors[4],
+        tension: .4,
+        yAxisID: 'y',
+      },
+      {
+        label: 'Productos',
+        data: prods,
+        backgroundColor: bg_colors[0],
+        borderColor: colors[0],
+        tension: .4,
+        yAxisID: 'y1',
       },
     ]
   }
 
   const options = {
     responsive: true,
-    legend: {
-      display: false
-    },
-    title: {
-      display: true,
-      text: 'Ventas x Hora'
+    scales: {
+      y: {
+        
+        display: true,
+        position: 'left',
+      },
+      y1: {
+        display: true,
+        position: 'right',
+        ticks: {
+          beginAtZero: true,
+        },
+      },
     }
-  }
+  };
 
   new Chart('main_chart', { type: 'line', data, options }, )
 }
